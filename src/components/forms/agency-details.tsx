a1e712acd00b2b34cfbf6de94d78775e500dd8dd
kamrulsaad <skamrul2680@gmail.com>
2024-11-11 06:56:33 +0600

    FEAT: Create Agency Queries

---
+++ src/components/forms/agency-details.tsx
@@ -0,0 +1,12 @@
+import { Agency } from "@prisma/client";
+import React from "react";
+
+type Props = {
+  data?: Partial<Agency>;
+};
+
+const AgencyDetails = ({ data }: Props) => {
+  return <div>AgencyDetails</div>;
+};
+
+export default AgencyDetails;
