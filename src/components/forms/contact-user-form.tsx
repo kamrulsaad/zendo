612c6b08e78c9fc776e9a986a355e35aa72e9f8e
kamrulsaad <skamrul2680@gmail.com>
2025-01-18 19:56:58 +0600

    Minor Fixes

--- src/components/forms/contact-user-form.tsx
+++ src/components/forms/contact-user-form.tsx
@@ -68,7 +68,7 @@ const ContactUserForm: React.FC<ContactUserFormProps> = ({ subaccountId }) => {
       })
       toast({
         title: 'Success',
-        description: 'Saved funnel details',
+        description: 'Saved Contact details',
       })
       setClose()
       router.refresh()
@@ -76,7 +76,7 @@ const ContactUserForm: React.FC<ContactUserFormProps> = ({ subaccountId }) => {
       toast({
         variant: 'destructive',
         title: 'Oppse!',
-        description: 'Could not save funnel details',
+        description: 'Could not save Contact details',
       })
     }
   }
