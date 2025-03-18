eed87ea0d5d7dbc63b6abad6bdb748e5cf6b003f
kamrulsaad <skamrul2680@gmail.com>
2025-03-19 00:36:29 +0600

    CTA section Landing Page done

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,20 +1,13 @@
 import { BackgroundBeams } from "@/components/modules/landing/BackgroundBeams";
 import { HeroContainerScroll } from "@/components/modules/landing/HeroContainerScroll";
 import HoverPriceCard from "@/components/modules/landing/HoverPriceCard";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { InfiniteMovingCards } from "@/components/modules/landing/InfiniteMovingCards";
+import { StickyScroll } from "@/components/modules/landing/StickyScroll";
 import MaxWidthWrapper from "@/components/ui/max-width-wrapper";
-import { pricingCards } from "@/lib/constants";
 import { stripe } from "@/lib/stripe";
-import clsx from "clsx";
-import { Check } from "lucide-react";
 import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 export default async function Home() {
   const prices = await stripe.prices.list({
@@ -45,6 +38,56 @@ export default async function Home() {
           <HoverPriceCard />
         </MaxWidthWrapper>
       </section>
+
+      {/* Testimonials Section  */}
+      <section className="w-full mt-10 md:mt-20">
+        <InfiniteMovingCards pauseOnHover={false} speed="slow" />
+      </section>
+
+      {/* Features Section  */}
+      <section className="w-full mt-10 md:mt-20">
+        <MaxWidthWrapper>
+          <div className="flex flex-col gap-4 items-center">
+            <h2 className="text-4xl text-center font-medium">
+              Explore new features
+            </h2>
+            <div className="text-muted-foreground text-center">
+              <p>
+                Zendo does everything possible to provide you with a convenient
+                tool for managing your agency.
+              </p>
+              <p>Here are just a few tools that may interest you.</p>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+        <div className="py-10">
+          <StickyScroll />
+        </div>
+      </section>
+
+
+      <div className="h-[40rem] w-full rounded-md relative flex flex-col items-center justify-center antialiased">
+        <div className="max-w-2xl mx-auto p-4">
+          <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+            Join Zendo
+          </h1>
+          <p></p>
+          <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+            Discover the power of seamless agency management with Zendo Agency
+            CRM. Experience the difference today and revolutionize the way you
+            manage your agency with Zendo.
+          </p>
+          <div className="flex justify-center mt-8">
+            <Link
+              href="/agency"
+              className={cn(buttonVariants({ variant: "secondary" }), "w-20 cursor-pointer")}
+            >
+              Join
+            </Link>
+          </div>
+        </div>
+        <BackgroundBeams />
+      </div>
     </div>
   );
 }
